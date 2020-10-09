ff4b77f41e2fd8d912957ec4831b51668507047b
Basic66 <47943912+Basic66@users.noreply.github.com>
2020-10-09 20:03:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+
+
+# Xorg provides a base for a graphical environment
+sudo apt install xorg
+
+lightdm
+=======
+
+# login manager
+sudo apt install lightdm
+
+i3-gaps
+=======
+
+# Dependencies
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
+
+# Clone the repository
+git clone https://www.github.com/Airblader/i3 .i3-gaps
+cd .i3-gaps
+
+# Compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make -j8
+sudo make install
