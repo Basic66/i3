bd48feefec33483029b4a1f690573dab06f53391
Basic66 <47943912+Basic66@users.noreply.github.com>
2023-07-31 13:16:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ i3-gaps
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
 
 # Clone the repository
-git clone https://www.github.com/Airblader/i3 .i3-gaps
+git clone https://www.github.com/resloved/i3 .i3-gaps
 cd .i3-gaps
 
 # Compile & install
